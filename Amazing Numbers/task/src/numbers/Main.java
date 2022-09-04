0c54286f07b4e9f8cf1d55d7b28f5b287972befb
Nikola Saudino <85690880+DinoChunk@users.noreply.github.com>
2022-09-04 17:38:11 +0200

    Stage 2/8

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -10,45 +10,36 @@ public class Main {
         System.out.println("Enter a natural number:");
         Scanner sc = new Scanner(System.in);
         if (!sc.hasNextInt()) {
-            System.out.println("This number is not natural!");
+            System.out.println("This number is not an integer!");
             return;
         }
 
         int num = sc.nextInt();
+
         boolean natural = num > 0;
         if (!natural) {
             System.out.println("This number is not natural!");
             return;
         }
         boolean even = num % 2 == 0;
-        if (even) {
-            System.out.println("This number is Even.");
-        } else {
-            System.out.println("This number is Odd.");
+        boolean buzz = num % 10 == 7 || num % 7 == 0;
+        boolean duck = false;
+
+        int duckCheck = num;
+        while (duckCheck != 0) {
+            if (duckCheck % 10 == 0) {
+                duck = true;
+                break;
+            }
+            duckCheck /= 10;
         }
 
-        boolean buzz1 = num % 7 == 0;
-        boolean buzz2 = num % 10 == 7;
+        System.out.printf("Properties of %d%n" +
+                "%13s%b%n" +
+                "%13s%b%n" +
+                "%13s%b%n" +
+                "%13s%b%n", num, "even:", even, "odd:", !even, "buzz:", buzz, "duck:", duck);
 
-        if (buzz1 || buzz2) {
-            System.out.println("It is a Buzz number.");
-        } else {
-            System.out.println("It is not a Buzz number.");
-        }
-
-        if (buzz1 && !buzz2) {
-            System.out.printf("Explanation:\n%d is divisible by 7", num);
-            return;
-        }
-        if (buzz2 && !buzz1) {
-            System.out.printf("Explanation:\n%d ends with 7", num);
-            return;
-        }
-        if (buzz2 && buzz1) {
-            System.out.printf("Explanation:\n%d is divisible by 7 and ends with 7", num);
-        } else {
-            System.out.printf("Explanation:\n%d is neither divisible by 7 nor does it end with 7", num);
-        }
     }
 }
 
